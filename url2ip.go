d74816a6bc852a890b99db0eb8b425e404826f2d
Steven Braham <steven@braham.biz>
2017-07-04 00:07:50 +0200

    working prototype

--- url2ip.go
+++ url2ip.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func main() {
 
-	var url = os.Args[1]
-	fmt.Println(url)
+	url := os.Args[1]
+	domain := getDomainFromUrl(url)
+
+	//execute ping request
+	out, _ := exec.Command("ping", "-n", "-c1", domain).Output()
+
+	//parse ping output
+	regex := regexp.MustCompile(`(\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3})`)
+
+	fmt.Println(regex.FindString((string(out))))
+}
+
+func getDomainFromUrl(url string) string {
+	regex := regexp.MustCompile(`\:\/\/(.+\.\w+)`)
+	domain := regex.FindString(url)
+	domain = strings.Replace(domain, "://", "", 2)
+	return domain
 }
